7c2e39977849d45429cac94eb09a1d0d70af3825
Konstantiss <konstantinos.michopoulos@gmail.com>
2024-11-09 17:07:57 +0200

    Add simulation.

--- init.py
+++ init.py
@@ -1,5 +1,3 @@
-from audioop import reverse
-
 import pandas as pd
 
 NUM_OF_RACKS = 2
@@ -85,6 +83,7 @@ def add_pallet(rack, bay_id, pallet):
 
     return "No available space in bay."
 
+
 def retrieve_pallet(racks, category):
     rack_ids = []
     bay_ids = []
@@ -102,7 +101,7 @@ def retrieve_pallet(racks, category):
     shelf_ids = [i for i in shelf_ids if i is not None]
     pallets = [i for i in pallets if i is not None]
 
-    if len(bay_ids) > 1: #If there are > 1 candidate pallets
+    if len(bay_ids) > 1:  # If there are > 1 candidate pallets
         retrieval_times = []
         distances_covered = []
         for index in range(len(bay_ids)):
@@ -110,18 +109,21 @@ def retrieve_pallet(racks, category):
             retrieval_times.append(retrieval_time)
             distances_covered.append(distance_covered)
         optimal_index = retrieval_times.index(min(retrieval_times))
-        racks[optimal_index].bays[bay_ids[optimal_index]].shelves[shelf_ids[optimal_index]].remove_pallet(pallets[optimal_index])
-        return retrieval_times[optimal_index], distances_covered[optimal_index]
+        racks[optimal_index].bays[bay_ids[optimal_index]].shelves[shelf_ids[optimal_index]].remove_pallet(
+            pallets[optimal_index])
+        return (
+            f"Retrieved pallet of {pallets[optimal_index].category} from rack {racks[optimal_index].rack_id + 1} bay "
+            f"{bay_ids[optimal_index] + 1} shelf {shelf_ids[optimal_index] + 1}."), \
+            retrieval_times[optimal_index], distances_covered[optimal_index]
     elif len(bay_ids) == 1:
         retrieval_time, distance_covered = calculate_retrieval_time(bay_ids[0], shelf_ids[0])
         racks[rack_ids[0]].bays[bay_ids[0]].shelves[shelf_ids[0]].remove_pallet(pallets[0])
-        return retrieval_time, distance_covered
+        return f"Retrieved pallet of {pallets[0].category} from rack {rack_ids[0] + 1} bay {bay_ids[0] + 1} shelf {shelf_ids[0] + 1}", retrieval_time, distance_covered
     else:
         return "Pallet not found."
 
 
 def search_pallet_in_rack(rack, category):
-
     if category == 'Category A':
         '''
         Reverse loop the bay list because of the proximity to the pick-up area.
@@ -149,12 +151,14 @@ def search_pallet_in_rack(rack, category):
 
     return None, None, None, None
 
+
 def calculate_placement_time(bay_id, shelf_id):
     distance_to_bay = DROP_OFF_TO_AISLE_DISTANCE + bay_id * BAY_WIDTH + BAY_WIDTH / 2
     move_time = distance_to_bay / FORKLIFT_MOVE_SPEED
     lift_time = (shelf_id * SHELF_HEIGHT) / FORKLIFT_LIFT_SPEED
     total_time = move_time + lift_time + PLACEMENT_TIME
     return total_time, distance_to_bay
+
 
 def calculate_retrieval_time(bay_id, shelf_id):
     distance_to_bay = PICK_UP_TO_AISLE_DISTANCE + bay_id * BAY_WIDTH + BAY_WIDTH / 2
@@ -163,73 +167,85 @@ def calculate_retrieval_time(bay_id, shelf_id):
     total_time = move_time + lift_time + RETRIEVAL_TIME
     return total_time, distance_to_bay
 
-def add_preexisting_stock(racks, inputs, outputs):
 
-    diff = inputs['Category'].value_counts()['Category A'] - outputs['Category'].value_counts()['Category A']
-    if diff < 0:
+def add_preexisting_stock(racks, pallets_to_add):
+
+    for rack in racks:
         pallet_count = 0
-        for rack in racks:
-            for bay in reversed(rack.bays):
-                for index in range(bay.shelves[0].maxNumOfPallets): #Fill the bottom shelves
-                    if pallet_count == abs(diff):
-                        break
-                    else:
-                        pallet = Europallet(category='Category A')
-                        add_pallet(rack, bay.bay_id, pallet)
-                        pallet_count += 1
+        for bay in reversed(rack.bays):
+            if pallet_count == pallets_to_add:
+                break
+            else:
+                pallet = Europallet(category='Category A')
+                add_pallet(rack, bay.bay_id, pallet)
+                pallet_count += 1
 
-    diff = inputs['Category'].value_counts()['Category B'] - outputs['Category'].value_counts()['Category B']
-
-    if diff < 0:
+    for rack in racks:
         pallet_count = 0
-        for rack in racks:
-            for bay in reversed(rack.bays):
-                for index in range(bay.shelves[-1].maxNumOfPallets):  # Fill the top shelves
-                    if pallet_count == abs(diff):
-                        break
-                    else:
-                        pallet = Europallet(category='Category B')
-                        add_pallet(rack, bay.bay_id, pallet)
-                        pallet_count += 1
+        for bay in reversed(rack.bays):
+            if pallet_count == pallets_to_add:
+                break
+            else:
+                pallet = Europallet(category='Category B')
+                add_pallet(rack, bay.bay_id, pallet)
+                pallet_count += 1
 
-    diff = inputs['Category'].value_counts()['Category C'] - outputs['Category'].value_counts()['Category C']
-
-    if diff < 0:
+    for rack in racks:
         pallet_count = 0
-        for rack in racks:
-            for bay in reversed(rack.bays):
-                for shelf in bay.shelves:
-                    for index in range(shelf.maxNumOfPallets):
-                        if pallet_count == abs(diff):
-                            break
-                        else:
-                            pallet = Europallet(category='Category C')
-                            add_pallet(rack, bay.bay_id, pallet)
-                            pallet_count += 1
+        for bay in reversed(rack.bays):
+            if pallet_count == pallets_to_add:
+                break
+            else:
+                pallet = Europallet(category='Category C')
+                add_pallet(rack, bay.bay_id, pallet)
+                pallet_count += 1
+
 
 inputs = pd.read_csv('warehouse_log_inputs.csv')
 outputs = pd.read_csv('warehouse_log_outputs.csv')
 racks = [Rack(id=i) for i in range(NUM_OF_RACKS)]
 
 inputs['Date'] = pd.to_datetime(inputs['Date'], format="%d/%m/%Y")
+outputs['Date'] = pd.to_datetime(outputs['Date'], format="%d/%m/%Y")
 
 total_placement_time = 0
-total_distance_covered = 0
+total_retrieval_time = 0
+total_placement_distance_covered = 0
+total_retrieval_distance_covered = 0
 
-add_preexisting_stock(racks, inputs, outputs)
+add_preexisting_stock(racks, pallets_to_add=9)
 
-for date, day_data in inputs.groupby(inputs['Date'].dt.date):
+for date, inputs_day_data in inputs.groupby(inputs['Date'].dt.date):
     day_placement_time = 0
-    day_distance_covered = 0
-    for index, row in day_data.iterrows():
+    day_retrieval_time = 0
+    day_distance_covered_placement = 0
+    day_distance_covered_retrieval = 0
+    outputs_day_data = outputs[outputs['Date'] == inputs_day_data['Date'].iloc[0]]
+
+    for index, row in inputs_day_data.iterrows():
         rack = int(row['Rack'].split('Rack ')[1])  # Get only the number from 'Rack X'
         bay = int(row['Bay'])
         category = row['Category']
         pallet = Europallet(category=category)
-        day_placement_time += add_pallet(racks[rack - 1], bay - 1, pallet)[1]
-        day_distance_covered += add_pallet(racks[rack - 1], bay - 1, pallet)[2]
-    retrieve_pallet(racks, category='Category A')
+        _, placement_time, distance_covered = add_pallet(racks[rack - 1], bay - 1, pallet)
+        day_placement_time += placement_time
+        day_distance_covered_placement += distance_covered
+
+    for index, row in outputs_day_data.iterrows():
+        category = row['Category']
+        _, retrieval_time, distance_covered = retrieve_pallet(racks, category)
+
+        day_retrieval_time += retrieval_time
+        day_distance_covered_retrieval += distance_covered
+
     total_placement_time += day_placement_time
-    total_distance_covered += day_distance_covered
+    total_retrieval_time += day_retrieval_time
+    total_placement_distance_covered += day_distance_covered_placement
+    total_retrieval_distance_covered += day_distance_covered_retrieval
     print(f"Processed data for {date}")
-    break
+
+print(f"Total placement time: {total_placement_time / 60} minutes")
+print(f"Total retrieval time: {total_retrieval_time / 60} minutes")
+print(f"Total placement distance covered: {total_placement_distance_covered} meters")
+print(f"Total retrieval distance covered: {total_retrieval_distance_covered} meters")
+print(f"Total distance covered: {total_placement_distance_covered + total_retrieval_distance_covered} meters")
